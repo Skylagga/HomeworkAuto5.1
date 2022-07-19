5807bd0bf67d06308a9b4fe407f76dcbe699d959
Skylagga <mike4797@icloud.com>
2022-07-19 11:40:11 +0300

    Test fix

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -22,7 +23,7 @@ public class AppCardDeliveryTest {
     @Test
     public void shouldMakeAppointmentAndNewDateOfAppointment() {
         $("[data-test-id='city'] input").val(user.getCity());
-        $("[data-test-id='date'] input").doubleClick().sendKeys("Backspace");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").val(DataGenerator.generateDate(0));
         $("[data-test-id='name'] input").val(user.getName());
         $("[data-test-id='phone'] input").val(user.getPhone());
@@ -31,7 +32,7 @@ public class AppCardDeliveryTest {
         $("[data-test-id='success-notification']").shouldBe(appear, Duration.ofSeconds(3));
         $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на " + DataGenerator.generateDate(0)));
         $x(".//button").click();
-        $("[data-test-id='date'] input").doubleClick().sendKeys("Backspace");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").val(DataGenerator.generateDate(4));
         $(byText("Запланировать")).click();
         $("[data-test-id='success-notification']").shouldBe(appear, Duration.ofSeconds(3));
